7fe7e73231998cba2f11f80978967de90c501fa8
AA-HAMZA <ahmed.rk30@gmail.com>
2022-03-28 19:13:33 +0200

    Only boot.iso

--- kernel/kernel_entry.asm
+++
@@ -1,6 +0,0 @@
-[bits 32]
-[extern _start]
-call _start
-jmp $
-
-%include "./interrupts.asm"
--- kernel/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_H
-#define TIMER_H
-
-#include "common.h"
-void init_timer(u32int frequency);
-
-typedef void (*function_ptr)(u32int);
-void add_func_to_timer(function_ptr);
-#endif
